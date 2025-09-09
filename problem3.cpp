3b1c54737ba70c89c1522ac4e520d1a9bca3c123
gulnur-urgenchbaeva <250310@newuu.uz>
2025-09-09 00:59:39 -0700

    [problem3]

---
+++ problem3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    cout << "I'm standing on the edge of some crazy cliff. What I have to do, I have to catch everybody\n if they start to go over the cliff - I mean\n if they're running and they don't look where they're going I have to come out from somewhere and catch them." << endl;
+    return 0;
+}
